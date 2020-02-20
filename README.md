b83625f630a296dbb22ecd9d476f5d1a4b1f7743
francesco.lacriola <chiccodallacinacolfurgone@yahoo.it>
2020-02-20 10:02:46 +0100

    Added Internalization

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 <h2>Done</h2>
 
-73 -> 79 da vedere
+73 -> 79. 86 -> 95 da vedere
 map.entrySet().removeIf(entry -> entry.getValue().equals(object)); <br>
 Java Streams to order elements
+@SpringBootTest(classes = SfgPetClinicApplication.class)
 
+Maven Spring Pet Clinic - Clean -> Package
 <ul>
     <li>85 (Section 5)</li>
 </ul>
---
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package sfgpetclinic;
+
+import com.francescolacriola.sfgpetclinic.SfgPetClinicApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SfgPetClinicApplication.class)
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
