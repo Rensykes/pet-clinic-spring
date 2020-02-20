ad01eef94548474fb480c6e9841dc8e8c54177da
francesco.lacriola <chiccodallacinacolfurgone@yahoo.it>
2020-02-20 10:27:43 +0100

    Added PetTypeService

---
+++ pet-clinic-data/src/main/java/com/francescolacriola/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.francescolacriola.sfgpetclinic.services;
+
+import com.francescolacriola.sfgpetclinic.model.PetType;
+import com.francescolacriola.sfgpetclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
